3f7c216c6a1449456046517d5b22284462babe4c
YiBiu <1551645539@qq.com>
2019-04-25 14:05:21 +0800

    modify knn

--- 《机器学习实战》Python/第二章 K-近邻算法/KNN.py
+++ 《机器学习实战》Python/第二章 K-近邻算法/KNN.py
@@ -14,10 +14,10 @@ import os
 #   class_data[in][list]: 样本对应的类别
 #   k[in]: KNN中的k，即前k个最近距离
 #
-def knn_classify(sample_data, train_data, class_data, k):
+def knn_classify(sample_data, train_datas, class_datas, k):
     # 计算欧式距离：sqrt[(x1 - x2)^2 + (y1 - y2)^2 + ...]
-    row_count = train_data.shape[0]
-    diff_data = tile(sample_data, (row_count, 1)) - train_data
+    row_count = train_datas.shape[0]
+    diff_data = tile(sample_data, (row_count, 1)) - train_datas
     diff_data_sqrt = diff_data ** 2
     diff_data_distances = diff_data_sqrt.sum(axis=1)
     euclid_distances = diff_data_distances ** 0.5
@@ -28,7 +28,7 @@ def knn_classify(sample_data, train_data, class_data, k):
     # 统计前k个距离最近样本的类别
     class_count = {}
     for i in range(k):
-        class_type = class_data[sorted_indexes[i]]
+        class_type = class_datas[sorted_indexes[i]]
         class_count[class_type] = class_count.get(class_type, 0) + 1
 
     # 输出最多类别为测试实例类别
@@ -39,14 +39,14 @@ def knn_classify(sample_data, train_data, class_data, k):
 ######################################################################
 # 示例 1: 电影分类
 def create_movie_datas():
-    train_movie_data = array([[1, 101], [5, 89], [108, 5], [115, 8]])
-    class_movie_data = ["love", "love", "action", "action"]
-    return train_movie_data, class_movie_data
+    train_movie_datas = array([[1, 101], [5, 89], [108, 5], [115, 8]])
+    class_movie_datas = ["love", "love", "action", "action"]
+    return train_movie_datas, class_movie_datas
 
 
 def movie_run():
-    movie_train_data, movie_class_data = create_movie_datas()
-    movie_result = knn_classify(array([101, 20]), movie_train_data, movie_class_data, 3)
+    movie_train_datas, movie_class_datas = create_movie_datas()
+    movie_result = knn_classify(array([101, 20]), movie_train_datas, movie_class_datas, 3)
     print(movie_result)
 
 
@@ -60,37 +60,37 @@ def file_to_matrix(file_name):
     fp = open(file_name)
     lines = fp.readlines()
     line_count = len(lines)
-    train_data = zeros((line_count, 3))
-    class_data = []
+    train_datas = zeros((line_count, 3))
+    class_datas = []
     index = 0
     for line in lines:
         line = line.strip()
         elems = line.split('\t')
-        train_data[index, :] = elems[0:3]
-        class_data.append(elems[-1])
+        train_datas[index, :] = elems[0:3]
+        class_datas.append(elems[-1])
         index += 1
-    return train_data, class_data
+    return train_datas, class_datas
 
 
 #
 # 说明：
 #   特征归一化
 #
-def auto_normalization(data):
+def auto_normalization(datas):
     # 计算最值
-    min_value = data.min(0)
-    max_value = data.max(0)
+    min_value = datas.min(0)
+    max_value = datas.max(0)
     ranges = max_value - min_value
     # 归一化：value = (value - min) / (max - min)
-    normal = data - tile(min_value, (data.shape[0], 1))
-    normal = normal / tile(ranges, (data.shape[0], 1))
+    normal = datas - tile(min_value, (datas.shape[0], 1))
+    normal = normal / tile(ranges, (datas.shape[0], 1))
     return normal
 
 
 def dating_run():
     # 加载数据并归一化
-    train_dating_data, class_dating_data = file_to_matrix("./datingTestSet.txt")
-    normal_data = auto_normalization(train_dating_data)
+    train_dating_datas, class_dating_datas = file_to_matrix("./datingTestSet.txt")
+    normal_data = auto_normalization(train_dating_datas)
 
     # 留出法分割数据为训练集和测试集
     # 测试集：test[0:test_num]；训练集train[test_num:]
@@ -101,10 +101,10 @@ def dating_run():
     error_count = 0.0
     for i in range(test_num):
         class_result = knn_classify(normal_data[i, :], normal_data[test_num:, :],
-                                    class_dating_data[test_num:], 3)
-        if class_result != class_dating_data[i]:
+                                    class_dating_datas[test_num:], 3)
+        if class_result != class_dating_datas[i]:
             error_count += 1.0
-        print("the classifier came back with: %s, the real answer is: %s" % (class_result, class_dating_data[i]))
+        print("the classifier came back with: %s, the real answer is: %s" % (class_result, class_dating_datas[i]))
     print("the total error rate is: %f" % (error_count / float(test_num)))
 
 
@@ -129,13 +129,13 @@ def hand_run():
     # 加载训练数据集
     train_file_list = os.listdir("trainingDigits")
     train_file_num = len(train_file_list)
-    train_hand_data = zeros((train_file_num, 1024))
-    class_hand_data = []
+    train_hand_datas = zeros((train_file_num, 1024))
+    class_hand_datas = []
     for i in range(train_file_num):
         file_name = train_file_list[i].split(".")[0]
         class_type = int(file_name.split("_")[0])
-        class_hand_data.append(class_type)
-        train_hand_data[i, :] = image_to_vector("trainingDigits/%s" % train_file_list[i])
+        class_hand_datas.append(class_type)
+        train_hand_datas[i, :] = image_to_vector("trainingDigits/%s" % train_file_list[i])
 
     # 加载测试数据集, 并依次测试统计
     test_file_list = os.listdir("testDigits")
@@ -145,7 +145,7 @@ def hand_run():
         file_name = test_file_list[i].split(".")[0]
         class_type = int(file_name.split("_")[0])
         test_vector = image_to_vector("testDigits/%s" % test_file_list[i])
-        class_result = knn_classify(test_vector, train_hand_data, class_hand_data, 3)
+        class_result = knn_classify(test_vector, train_hand_datas, class_hand_datas, 3)
         if class_result != class_type:
             error_count += 1.0
         print("the classifier came back with: %d, the real answer is: %d" % (class_result, class_type))
